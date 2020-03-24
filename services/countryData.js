f55923957940d28f0df8a910c58e9b558f827251
anupkum100 <anup.kumar@here.com>
2020-03-25 01:25:54 +0530

    added india map

--- services/countryData.js
+++ services/countryData.js
@@ -5,7 +5,7 @@ app.controller('myCtrl', function ($scope, $http) {
     $scope.indiaData = {};
     $scope.indainStateData = {};
     $scope.isCountrySelected = false;
-    $scope.isApiCallInProgress = true;
+    $scope.isApiCallInProgress = false;
 
     $scope.category = [{
         label: 'Total Cases',
@@ -27,6 +27,10 @@ app.controller('myCtrl', function ($scope, $http) {
 
     getIndianStateData();
 
+    getData("https://corona.lmao.ninja/countries");
+
+
+    // todo : find new api for world data
     $http({
         method: "GET",
         url: "https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php",
@@ -54,42 +58,19 @@ app.controller('myCtrl', function ($scope, $http) {
     });
 
     function getPercentage(category, total) {
-        let number = parseInt((category).replace(',', '')) / parseInt((total).replace(',', '')) * 100;
+        let number = category / total
         return number.toFixed(2)
     }
-
-    $scope.isApiCallInProgress = true;
-
-    $http({
-        method: "GET",
-        url: "https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php",
-        headers: {
-            "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
-            "x-rapidapi-key": "1c6e772606msh3c10d4d01ae157bp1dee68jsne1d22692e58a"
-        }
-    }).then(function mySuccess(response) {
-        $scope.isApiCallInProgress = false;
-        $scope.countryWise = response.data.countries_stat;
-
-        response.data.countries_stat.forEach(countryData => {
-            if (countryData.country_name === 'India') {
-                $scope.indiaData = countryData
-                $scope.changeArea($scope.indiaData);
-            }
-        })
-    }, function myError(response) {
-        $scope.myWelcome = response.statusText;
-    });
 
     $scope.showCountryList = () => {
         $scope.isCountrySelected = !$scope.isCountrySelected;
     }
 
-    $scope.changeArea = (country) => {
-        $scope.selectedArea = country.country_name;
+    $scope.changeArea = (countryData) => {
+        $scope.selectedArea = countryData.country;
         $scope.showCountryList();
-        if (country === 'World') {
-            $scope.selectedArea = country;
+        if (countryData === 'World') {
+            $scope.selectedArea = countryData;
             $scope.category[0].value = $scope.worldData.total_cases;
             $scope.category[1].value = $scope.worldData.total_deaths;
             $scope.category[2].value = $scope.worldData.total_recovered;
@@ -99,10 +80,8 @@ app.controller('myCtrl', function ($scope, $http) {
             $scope.category[2].percentage = getPercentage($scope.worldData.total_recovered, $scope.worldData.total_cases);
             $scope.category[3].percentage = getPercentage($scope.worldData.new_cases, $scope.worldData.total_cases);
 
-            createPieChart();
-
         } else {
-            updateAllData(country);
+            updateAllData(countryData);
         }
 
     }
@@ -125,58 +104,25 @@ app.controller('myCtrl', function ($scope, $http) {
     }
 
     function updateAllData(countryData) {
+        $scope.selectedLocationImportantData = {
+            deaths: countryData.deaths,
+            total: countryData.cases
+        };
         $scope.category[0].value = countryData.cases;
         $scope.category[1].value = countryData.deaths;
-        $scope.category[2].value = countryData.total_recovered;
-        $scope.category[3].value = countryData.new_cases;
+        $scope.category[2].value = countryData.recovered;
+        $scope.category[3].value = countryData.todayCases;
 
         $scope.category[1].percentage = getPercentage(countryData.deaths, countryData.cases);
-        $scope.category[2].percentage = getPercentage(countryData.total_recovered, countryData.cases);
-        $scope.category[3].percentage = getPercentage(countryData.new_cases, countryData.cases);
+        $scope.category[2].percentage = getPercentage(countryData.todayCases, countryData.cases);
+        $scope.category[3].percentage = getPercentage(countryData.todayCases, countryData.cases);
 
         getHistoryData();
         window.scrollTo(0, 0);
     }
 
-    function createPieChart() {
-        var ctx = document.getElementById("myPieChart");
-        totalPercentage = 100 - $scope.category[1].percentage - $scope.category[2].percentage - $scope.category[3].percentage;
-
-        return new Chart(ctx, {
-            type: 'doughnut',
-            data: {
-                labels: [$scope.category[0].label, $scope.category[1].label, $scope.category[2].label, $scope.category[3].label],
-                datasets: [{
-                    data: [totalPercentage, $scope.category[1].percentage, $scope.category[2].percentage, $scope.category[3].percentage],
-                    backgroundColor: ['#abcabc', '#4e73df', '#1cc88a', '#e74a3b'],
-                    hoverBackgroundColor: ['#000', '#000', '#000', '#000'],
-                    hoverBorderColor: "rgba(234, 236, 244, 1)",
-                }],
-            },
-            options: {
-                maintainAspectRatio: true,
-                tooltips: {
-                    backgroundColor: "rgb(255,255,255)",
-                    bodyFontColor: "#858796",
-                    borderColor: '#dddfeb',
-                    borderWidth: 1,
-                    xPadding: 15,
-                    yPadding: 15,
-                    displayColors: false,
-                    caretPadding: 10,
-                },
-                legend: {
-                    display: true
-                },
-                cutoutPercentage: 80,
-            },
-        });
-
-    }
-
     function createAreaChart() {
 
-        // Area Chart Example
         var ctx = document.getElementById("myAreaChart");
 
         let labels = $scope.graphData.map(historyData => historyData.data.record_date.split(' ')[0]);
@@ -190,28 +136,28 @@ app.controller('myCtrl', function ($scope, $http) {
                 labels: labels,
                 datasets: [{
                     label: "Infected",
-                    lineTension: 0.3,
+                    lineTension: 0.7,
                     backgroundColor: "rgba(78, 115, 223, 0.05)",
                     borderColor: "rgba(78, 115, 223, 1)",
-                    pointRadius: 3,
+                    pointRadius: 2,
                     pointBackgroundColor: "rgba(78, 115, 223, 1)",
                     pointBorderColor: "rgba(78, 115, 223, 1)",
-                    pointHoverRadius: 3,
+                    pointHoverRadius: 2,
                     pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
                     pointHoverBorderColor: "rgba(78, 115, 223, 1)",
-                    pointHitRadius: 10,
-                    pointBorderWidth: 2,
+                    pointHitRadius: 2,
+                    pointBorderWidth: 1,
                     data
                 },
                 {
                     label: "Deaths",
-                    lineTension: 0.3,
+                    lineTension: 0.7,
                     backgroundColor: "rgba(78, 115, 223, 0.05)",
                     borderColor: "#e74a3b",
-                    pointRadius: 3,
+                    pointRadius: 2,
                     pointBackgroundColor: "#e74a3b",
                     pointBorderColor: "#e74a3b",
-                    pointHoverRadius: 3,
+                    pointHoverRadius: 2,
                     pointHoverBackgroundColor: "#e74a3b",
                     pointHoverBorderColor: "#e74a3b",
                     pointHitRadius: 10,
@@ -220,63 +166,44 @@ app.controller('myCtrl', function ($scope, $http) {
                 },
                 {
                     label: "Recovered",
-                    lineTension: 0.3,
+                    lineTension: 0.7,
                     backgroundColor: "rgba(78, 115, 223, 0.05)",
                     borderColor: "#1cc88a",
-                    pointRadius: 3,
+                    pointRadius: 2,
                     pointBackgroundColor: "#1cc88a",
                     pointBorderColor: "#1cc88a",
-                    pointHoverRadius: 3,
+                    pointHoverRadius: 2,
                     pointHoverBackgroundColor: "#1cc88a",
                     pointHoverBorderColor: "#1cc88a",
                     pointHitRadius: 10,
                     pointBorderWidth: 2,
                     data: recovered
-                }],
+                }
+                ],
             },
             options: {
-                maintainAspectRatio: false,
-                layout: {
-                    padding: {
-                        left: 10,
-                        right: 25,
-                        top: 25,
-                        bottom: 0
-                    }
-                },
+                maintainAspectRatio: true,
                 scales: {
                     xAxes: [{
-                        time: {
-                            unit: 'date'
-                        },
                         gridLines: {
-                            display: false,
+                            display: true,
                             drawBorder: false
                         },
                         ticks: {
-                            maxTicksLimit: 10
+                            minTicksLimit: 10
                         }
                     }],
                     yAxes: [{
                         ticks: {
-                            maxTicksLimit: 10,
-                            padding: 10,
-                            // Include a dollar sign in the ticks
+                            maxTicksLimit: 20,
                             callback: function (value, index, values) {
-                                return value
+                                return value;
                             }
-                        },
-                        gridLines: {
-                            color: "rgb(234, 236, 244)",
-                            zeroLineColor: "rgb(234, 236, 244)",
-                            drawBorder: false,
-                            borderDash: [2],
-                            zeroLineBorderDash: [2]
                         }
                     }],
                 },
                 legend: {
-                    display: true
+                    display: false
                 },
                 tooltips: {
                     backgroundColor: "rgb(255,255,255)",
@@ -327,7 +254,6 @@ app.controller('myCtrl', function ($scope, $http) {
                 }
             })
             createAreaChart();
-            createPieChart();
         }, function myError(response) {
             $scope.myWelcome = response.statusText;
         });
@@ -337,6 +263,23 @@ app.controller('myCtrl', function ($scope, $http) {
         return {
             width: percentage + '%'
         }
+    }
+
+    function getData(url) {
+        $http({
+            method: "GET",
+            url
+        }).then(function mySuccess(response) {
+            $scope.isApiCallInProgress = false;
+            $scope.countryWise = response.data;
+            response.data.forEach(countryData => {
+                if (countryData.country === 'India') {
+                    $scope.indiaData = countryData
+                    $scope.changeArea($scope.indiaData);
+                }
+            })
+        }, function myError(response) {
+        });
     }
 
 
---
+++ services/service.js
@@ -0,0 +1,243 @@
+simplemaps_countrymap_mapdata = {
+    main_settings: {
+        //General settings
+        width: "responsive", //'700' or 'responsive'
+        background_color: "#FFFFFF",
+        background_transparent: "yes",
+        border_color: "#ffffff",
+
+        //State defaults
+        state_description: "State description",
+        state_color: "#88A4BC",
+        state_hover_color: "#3B729F",
+        border_size: 1.5,
+        all_states_inactive: "no",
+        all_states_zoomable: "no",
+
+        //Location defaults
+        location_description: "Location description",
+        location_color: "#FF0067",
+        location_opacity: 0.8,
+        location_hover_opacity: 1,
+        location_url: "",
+        location_size: 25,
+        location_type: "square",
+        location_image_source: "frog.png",
+        location_border_color: "#FFFFFF",
+        location_border: 2,
+        location_hover_border: 2.5,
+        all_locations_inactive: "no",
+        all_locations_hidden: "no",
+
+        //Label defaults
+        label_color: "#d5ddec",
+        label_hover_color: "#d5ddec",
+        label_size: 22,
+        label_font: "Arial",
+        hide_labels: "no",
+        hide_eastern_labels: "no",
+
+        //Zoom settings
+        zoom: "no",
+        back_image: "no",
+        initial_back: "no",
+        initial_zoom: "-1",
+        initial_zoom_solo: "no",
+        region_opacity: 1,
+        region_hover_opacity: 0.6,
+        zoom_out_incrementally: "yes",
+        zoom_percentage: 0.99,
+        zoom_time: 0.5,
+
+        //Popup settings
+        popup_color: "white",
+        popup_opacity: 0.9,
+        popup_shadow: 1,
+        popup_corners: 5,
+        popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+        popup_nocss: "no",
+
+        //Advanced settings
+        div: "map",
+        auto_load: "yes",
+        url_new_tab: "no",
+        images_directory: "default",
+        fade_time: 0.1,
+        link_text: "View Website",
+        popups: "detect",
+        state_image_url: "",
+        state_image_position: "",
+        location_image_url: "",
+        manual_zoom: "yes"
+    },
+    state_specific: {
+        "1": {
+            name: "Andaman and Nicobar",
+            description: "0"
+        },
+        "2": {
+            name: "Andhra Pradesh",
+            description: "0"
+        },
+        "3": {
+            name: "Arunachal Pradesh",
+            description: "0"
+        },
+        "4": {
+            name: "Assam",
+            description: "0"
+        },
+        "5": {
+            name: "Bihar",
+            description: "0"
+        },
+        "6": {
+            name: "Chandigarh",
+            description: "0"
+        },
+        "7": {
+            name: "Chhattisgarh",
+            description: "0"
+        },
+        "8": {
+            name: "Dadra and Nagar Haveli",
+            description: "0"
+        },
+        "9": {
+            name: "Daman and Diu",
+            description: "0"
+        },
+        "10": {
+            name: "Delhi",
+            description: "0"
+        },
+        "11": {
+            name: "Goa",
+            description: "0"
+        },
+        "12": {
+            name: "Gujarat",
+            description: "0"
+        },
+        "13": {
+            name: "Haryana",
+            description: "0"
+        },
+        "14": {
+            name: "Himachal Pradesh",
+            description: "0"
+        },
+        "16": {
+            name: "Jharkhand",
+            description: "0"
+        },
+        "17": {
+            name: "Karnataka",
+            description: "0"
+        },
+        "18": {
+            name: "Kerala",
+            description: "0"
+        },
+        "19": {
+            name: "Lakshadweep",
+            description: "0"
+        },
+        "20": {
+            name: "Madhya Pradesh",
+            description: "0"
+        },
+        "21": {
+            name: "Maharashtra",
+            description: "0"
+        },
+        "22": {
+            name: "Manipur",
+            description: "0"
+        },
+        "23": {
+            name: "Meghalaya",
+            description: "0"
+        },
+        "24": {
+            name: "Mizoram",
+            description: "0"
+        },
+        "25": {
+            name: "Nagaland",
+            description: "0"
+        },
+        "26": {
+            name: "Orissa",
+            description: "0"
+        },
+        "27": {
+            name: "Puducherry",
+            description: "0"
+        },
+        "28": {
+            name: "Punjab",
+            description: "0"
+        },
+        "29": {
+            name: "Rajasthan",
+            description: "0"
+        },
+        "30": {
+            name: "Sikkim",
+            description: "0"
+        },
+        "31": {
+            name: "Tamil Nadu",
+            description: "0"
+        },
+        "32": {
+            name: "Tripura",
+            description: "0"
+        },
+        "33": {
+            name: "Uttar Pradesh",
+            description: "0"
+        },
+        "34": {
+            name: "Uttaranchal",
+            description: "0"
+        },
+        "35": {
+            name: "West Bengal",
+            description: "0"
+        },
+        "36": {
+            name: "Jammu and Kashmir",
+            description: "0"
+        },
+        "37": {
+            name: "Telangana",
+            description: "0"
+        }
+    },
+    // locations: {
+    //     "0": {
+    //         lat: 18.987807,
+    //         lng: 72.836447,
+    //         name: "Mumbai",
+    //         state_description: "Mumbai"
+    //     }
+    // },
+    labels: {},
+    regions: {}
+};
+
+$.holdReady(true);
+$.get("https://ameerthehacker.github.io/corona-india-status/covid19-indian-states.json", function (response) {
+    $.holdReady(false);
+    Object.keys(simplemaps_countrymap_mapdata.state_specific).forEach((stateData, index) => {
+        if (response.data[simplemaps_countrymap_mapdata.state_specific[stateData].name]) {
+            simplemaps_countrymap_mapdata.state_specific[stateData].description = 'Total:' + response.data[simplemaps_countrymap_mapdata.state_specific[stateData].name].totalIndianCases + '</br>' + 
+            '<span class="text-danger">Deaths:' + response.data[simplemaps_countrymap_mapdata.state_specific[stateData].name].totalDeaths + '</span></br>' + 
+            '<span class="text-info">Recovered:' + response.data[simplemaps_countrymap_mapdata.state_specific[stateData].name].totalRecovered + '</span>'
+        }
+
+    })
+
+})
